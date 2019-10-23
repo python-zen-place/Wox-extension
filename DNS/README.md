8658ae09bded95ba04ab71e0cf0eb1c0545025de
Weilet <453546129@qq.com>
2019-10-23 11:23:31 +0800

    add DNS

---
+++ DNS/README.md
@@ -0,0 +1,14 @@
+### DNS Manager
+
+> DNS 管理器
+
+触发关键字: <br/>
+`dns list`  列出所有IP <br />
+`dns all`   测试所有DNS的速度 <br />
+`dns add {ip}#` 添加一个DNS  <br />
+`dns remove {ip}#` 删除一个DNS，点击对应DNS快速填充 <br />
+
+#### TODO:
+- [ ] 自实现ping
+- [ ] 利用pywin32实现点击设置DNS 
+
---
+++ DNS/main.py
@@ -0,0 +1,83 @@
+import os
+import re
+from wox import Wox, WoxAPI
+from dns import dns
+from multiprocessing import Pool
+
+
+def ping(server):
+    with os.popen('ping {} -4 -w 3000'.format(server)) as res:
+        data = res.read()
+        if 'ms' not in data:
+            return {
+                'Title': server,
+                'SubTitle': '超时',
+                'IcoPath': 'image/icon.png'
+            }
+        else:
+            time = data.split('=')[-1]
+            return {
+                'Title': server,
+                'SubTitle': time.strip(),
+                'IcoPath': 'image/icon.png'
+            }
+
+
+def list_factory(dns, JsonRPCActionMethod=None):
+    results = [{
+        'Title': 'DNS',
+        'SubTitle': server,
+        'IcoPath': 'image/icon.png',
+    } for server in dns]
+    if JsonRPCActionMethod:
+        for result in results:
+            result['JsonRPCAction'] = {
+                'method': Json,
+                'parameters': [result['SubTitle']],
+                'dontHideAfterAction': True
+            }
+    return results
+
+
+class DNS(Wox):
+    def query(self, query):
+        if query == 'list':
+            return list_factory(dns, 'test')
+        elif query == 'all':
+            p = Pool(20)
+            tasks = [p.apply_async(ping, args=(server,)) for server in dns]
+            p.close()
+            p.join()
+            return sorted([res.get() for res in tasks],
+                          key=lambda x: int(x['SubTitle'][:-2]) if x['SubTitle'] != '超时' else 10000)
+        elif query.startswith('add'):
+            ip = re.search('(\d{1,3}\.){3}\d{1,3}#', query)
+            if ip:
+                ip = ip.string.split()[1][:-1]
+                if ip not in dns:
+                    dns.append(ip)
+                    data = ''.join(['\'{}\','.format(server) for server in dns])
+                    with open('dns/__init__.py', 'w') as f:
+                        f.write('dns = [{}]'.format(data))
+            return list_factory(dns)
+        elif query.startswith('remove'):
+            ip = re.search('(\d{1,3}\.){3}\d{1,3}#', query)
+            if ip:
+                ip = ip.string.split()[1][:-1]
+                if ip in dns:
+                    data = ''.join(['\'{}\','.format(server) for server in dns if server != ip])
+                    with open('dns/__init__.py', 'w') as f:
+                        f.write('dns = [{}]'.format(data))
+            return list_factory(dns, 'remove')
+        elif re.match('(\d{1,3}\.){3}\d{1,3}#', query):
+            return [ping(query)]
+
+    def test(self, server):
+        WoxAPI.change_query('dns ' + server + '#')
+
+    def remove(self, server):
+        WoxAPI.change_query('dns remove ' + server + '#')
+
+
+if __name__ == '__main__':
+    DNS()
