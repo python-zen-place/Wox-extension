3e1b92f6474a65f21b0dc8c0adafeb33f2126a10
Weilet <453546129@qq.com>
2019-10-30 23:53:53 +0800

    Update main.py

--- woxpip/main.py
+++ woxpip/main.py
@@ -3,12 +3,21 @@ import os
 import requests
 from wox import Wox
 
+def run(command, package):
+    with os.popen('pip {command} {package}'.format(command, package)) as res:
+        data = res.split('\n')
+        return [{
+            'Title': 'ping',
+            'SubTitle': statement,
+            'IcoPath': 'image/icon.png'
+        } for statement in data]
+
 
 class WoxPip(Wox):
     def query(self, query):
-        if  query.endswith('#') and 'install' in query:
-            with os.popen('pip install {}'.format()):
-                pass
+        if query.endswith('#') and any(['install', 'remove', 'list']) in query:
+            pip, command, package = query.split()
+            return run(command, package)
 
 
 if __name__ == '__main__':
