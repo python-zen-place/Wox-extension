5be3c347c64655beafd80753ed86000e1d9d7ace
Weilet <453546129@qq.com>
2019-11-04 23:34:17 +0800

    update woxpip

--- woxpip/main.py
+++ woxpip/main.py
@@ -1,24 +1,40 @@
-from config import config
 import os
-import requests
 from wox import Wox
+from OperationError import OperationError
+
 
 def run(command, package):
-    with os.popen('pip {command} {package}'.format(command, package)) as res:
-        data = res.split('\n')
+    if 'ERROR' in res:
+        raise OperationError
+    if command == 'list':
+        with os.popen('pip list') as res:
+            data = res.split('\n')
+            return [{
+                'Title': 'ping',
+                'SubTitle': statement,
+                'IcoPath': 'image/icon.png'
+            } for statement in data]
+    elif command == 'uninstall':
+        res = os.popen('pip show {}'.format(package))
+        requires = res.split('Requires: ')[1].split('\n')[0].split(',')
         return [{
             'Title': 'ping',
-            'SubTitle': statement,
-            'IcoPath': 'image/icon.png'
-        } for statement in data]
+            'SubTitle': '',
+            'IcoPath': 'image/icon.png',
+            'JsonRPCAction': {
+                'method': '',
+                'parameters': '',
+                'dontHideAfterAction': True
+            }
+        }]
 
+        class WoxPip(Wox):
+            def query(self, query):
+                command = list(filter(lambda x: x in query, ['install', 'remove', 'list']))
+                assert len(command) == 1, "Command ERROR"
+                command = command[0]
+                pip, command, package = query.split()
+                return run(command, package)
 
-class WoxPip(Wox):
-    def query(self, query):
-        if query.endswith('#') and any(['install', 'remove', 'list']) in query:
-            pip, command, package = query.split()
-            return run(command, package)
-
-
-if __name__ == '__main__':
-    WoxPip()
+        if __name__ == '__main__':
+            WoxPip()
